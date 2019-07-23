c36dc2bfc3d7256974f83c671491a6fd7361902d
Johannes Kepper <kepper@edirom.de>
2019-07-23 15:10:22 +0200

    allow verovio as global variable, load it in plain

--- src/components/AnalysisPlain.vue
+++ src/components/AnalysisPlain.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="analysisPlain">
     <h1>AnalysisPlain</h1>
+    <div id="svgContainer"></div>
   </div>
 </template>
 
@@ -9,6 +10,71 @@
 export default {
   name: 'AnalysisPlain',
   components: {
+
+  },
+  mounted(){
+    console.log('me mounted')
+    this.getData()
+  },
+  methods: {
+    getData: function() {
+
+      let activeComparisonId = this.$store.getters.activeComparisonId
+      let activeMovement = this.$store.getters.activeMovement
+      let transpose = this.$store.getters.transpose
+
+      let api = process.env.VUE_APP_DATA_BACKEND_URL
+      let request = 'resources/xql/getAnalysis.xql?comparisonId=' + activeComparisonId + '&method=plain&mdiv=' + activeMovement + '&transpose=' + transpose
+
+      /*return 'api: ' + api + request*/
+
+      if(activeComparisonId === null) {
+        return false;
+      }
+
+      let options = {
+          scale: 50,
+          noFooter: 1, // takes out the "rendered by Verovio" footer
+          pageWidth: 500,
+          pageHeight: 500,
+          adjustPageHeight: true,
+          spacingNonLinear: 1,
+          spacingLinear: .05
+        };
+      try {
+          this.$verovio.setOptions(options);
+      } catch(err) {
+        console.log('ERR: ' + err)
+      }
+      console.log('successfully set options…')
+
+      // console.log('\nhello polly ' + typeof verovio)
+
+      new Promise(resolve => {
+        fetch(api + request)
+          .then(response => response.text()) // add error handling for failing requests
+          .then(mei => {
+            this.$verovio.loadData(mei + '\n')
+            this.loadPage(1)
+          })
+      })
+    },
+    loadPage: function(n) {
+      console.log('loading page ' + n)
+      let svg = this.$verovio.renderToSVG(n, {});
+      let svgContainer = document.querySelector('#svgContainer');
+      svgContainer.innerHTML = svg;
+
+
+    }
+  },
+  computed: {
+    activeModeId: function() {
+      return this.$store.getters.activeModeId
+    },
+    activeModeObject: function() {
+      return this.$store.getters.activeModeObject
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import './../node_modules/spectre.css/dist/spectre.min.css'
 import './../node_modules/spectre.css/dist/spectre-exp.min.css'
 import './../node_modules/spectre.css/dist/spectre-icons.min.css'
 
+// this allows verovio as global variable, c.f. https://forum.vuejs.org/t/single-file-component-how-to-access-imported-library-in-template/8850/2
+// this seems to be extremely slow while development, as it tries to compile Verovio every time…
+/* import vrvToolkit from './verovio.js'
+Vue.prototype.$verovio = vrvToolkit */
+
+const vrvToolkit = new verovio.toolkit()
+Vue.prototype.$verovio = vrvToolkit
+
 Vue.config.productionTip = false
 
 new Vue({
