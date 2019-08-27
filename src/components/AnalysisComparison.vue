1ca167354139fbe7e3834dada508451f6ca7e26f
Johannes Kepper <kepper@edirom.de>
2019-08-27 15:58:22 +0200

    first steps towards painting

--- src/components/AnalysisComparison.vue
+++ src/components/AnalysisComparison.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="analysisComparison">
     <h1>AnalysisComparison</h1>
+    <div id="svgContainer" class="comparison"></div>
   </div>
 </template>
 
 <script>
 
+import VerovioBaseComponent from './VerovioBaseComponent.vue'
+
 export default {
   name: 'AnalysisComparison',
+  extends: VerovioBaseComponent,
   components: {
+
+  },
+  methods: {
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 
+  $highlightColor_identical: #000000;
+  $highlightColor_different: #d825da;
+  $highlightColor_octDiff_durSame: #f4531b;
+  $highlightColor_octSame_durDiff: #26d6fc;
+  $highlightColor_octDiff_durDiff: #1b44f4;
+  $highlightColor_transp_durSame: #859900;
+
+  #svgContainer.comparison .noMatch {
+      fill: $highlightColor_different;
+      stroke: $highlightColor_different;
+  }
+
+  #svgContainer.comparison .ts {
+      fill: $highlightColor_transp_durSame;
+      stroke: $highlightColor_transp_durSame;
+  }
+
+  #svgContainer.comparison .od {
+      fill: $highlightColor_octDiff_durDiff;
+      stroke: $highlightColor_octDiff_durDiff;
+  }
+
+  #svgContainer.comparison .sd {
+      fill: $highlightColor_octSame_durDiff;
+      stroke: $highlightColor_octSame_durDiff;
+  }
+
+  #svgContainer.comparison .os, #svgContainer.comparison .os.od {
+      fill: $highlightColor_octDiff_durSame;
+      stroke: $highlightColor_octDiff_durSame;
+  }
+
+  #svgContainer.comparison .id {
+      fill: $highlightColor_identical !important;
+      stroke: $highlightColor_identical !important;
+  }
 </style>
--- src/components/AnalysisPlain.vue
+++ src/components/AnalysisPlain.vue
@@ -8,6 +8,8 @@
 
 import VerovioBaseComponent from './VerovioBaseComponent.vue'
 
+let paintableEvents = '#svgContainer .note, #svgContainer .rest, #svgContainer .slur'
+
 export default {
   name: 'AnalysisPlain',
   extends: VerovioBaseComponent,
@@ -15,12 +17,66 @@ export default {
 
   },
   methods: {
+    renderPage: function(n) {
 
+      this.removePageListeners()
+
+      // this calls the parent method and allows to have view-specific actions following the page being loaded
+      VerovioBaseComponent.methods.renderPage.call(this)
+
+      // custom actions start here
+      this.addPageListeners()
+    },
+
+    clickNote: function(e) {
+      console.log('clicked note')
+
+      let note = e.currentTarget
+      note.classList.add('color1')
+      /* note.style.fill = colors[activeColor];
+      note.style.stroke = colors[activeColor];
+
+      coloredNotes[note.id] = activeColor;  */
+    },
+
+    addPageListeners: function() {
+      let notes = document.querySelectorAll(paintableEvents)
+      notes.forEach((note,index,list) => {
+        note.addEventListener('click',this.clickNote,false)
+      })
+/*
+      //re-add colors
+      for (let noteId in coloredNotes) {
+        try {
+          let note = document.querySelector('#svgContainer #' + noteId);
+          let colorIndex = coloredNotes[noteId];
+
+          note.classList.add('color' + colorIndex);
+          note.style.fill = colors[colorIndex];
+          note.style.stroke = colors[colorIndex];
+
+        } catch(err) {
+          //console.log('[ERROR] Unable to (re-)color note ' + noteId + ': ' + err);
+        }
+      }    */
+
+    },
+
+    removePageListeners: function() {
+        let notes = document.querySelectorAll(paintableEvents)
+        notes.forEach((note,index,list) => {
+            note.removeEventListener('click',this.clickNote,false)
+        })
+        console.log('removed ' + notes.length + ' event listeners')
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
+<style lang="scss">
+  #svgContainer .color1 {
+    fill: #ff0000;
+    stroke: #ff0000;
+  }
 </style>
